57acf3253eb10455eed9e4d5f7927dab44410ffc
jnarmer-cmd <jnarmer@gmail.com>
2026-04-27 14:27:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker registrado"));
+  });
+}
